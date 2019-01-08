833b6538a1f8550e04e0ed0b3cd1c716b8206d79
Brian Rosengarten <barosengarten@gmail.com>
2019-01-08 06:01:50 +0000

    added methods

--- lib/nfl_news/cli.rb
+++ lib/nfl_news/cli.rb
@@ -1,7 +1,37 @@
 class NflNews::CLI
   
   def call
-    puts "Recent NFL Player News:"
+    list_players
+    choose
+    goodbye
   end
   
+  def list_players
+    puts "Recent NFL Player News:"
+    puts <<-eos
+ 1. Player 1 Name - Position - Team
+ 2. Player 1 Name - Position - Team
+    eos
+  end
+    
+  def choose
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the player whose news you'd like to view, type 'list' to re-list players, or 'exit':"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Player 1 news..."
+      when "2"
+        puts "Player 2 news..."
+      when "list"
+        call
+      end
+    end
+  end
+  
+  def goodbye
+    puts "Check back later for more news."
+  end
+    
 end
