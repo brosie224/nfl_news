65706056c31e3aa0958c1e788cebf3d35b447bc3
Brian Rosengarten <barosengarten@gmail.com>
2019-01-08 06:23:56 +0000

    updated Player class

--- lib/nfl_news/player.rb
+++ lib/nfl_news/player.rb
@@ -1,10 +1,26 @@
 class NflNews::Player
   
+  attr_accessor :name, :pos, :team, :url
+  
   def self.recent
         puts <<-eos
  1. Player 1 Name - Position - Team
  2. Player 1 Name - Position - Team
     eos
+    
+    player_1 = self.new
+    player_1.name = "Kerryon Johnson"
+    player_1.pos = "RB"
+    player_1.team = "Lions"
+    player_1.url = "http://www.rotoworld.com/player/nfl/13156/kerryon-johnson"
+    
+    player_2 = self.new
+    player_2.name = "D.J. Moore"
+    player_2.pos = "WR"
+    player_2.team = "Panthers"
+    player_2.url = "http://www.rotoworld.com/player/nfl/13114/dj-moore"
+    
+    [player_1, player_2]
   end
 
 end
