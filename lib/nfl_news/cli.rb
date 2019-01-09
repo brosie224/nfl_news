3f354fa972dc16dc6063463bcf39de2266047cf2
Brian Rosengarten <barosengarten@gmail.com>
2019-01-09 01:58:03 +0000

    adds spacing

--- lib/nfl_news/cli.rb
+++ lib/nfl_news/cli.rb
@@ -19,15 +19,22 @@ class NflNews::CLI
   def choose
     input = nil
     while input != "exit"
+      puts ""
       puts "Enter the number of the player whose news you'd like to view, 'list' to re-list players, or 'exit':"
       input = gets.strip.downcase
       
       if input.to_i > 0
+        puts ""
         puts @players[input.to_i-1][:news]
+        puts ""
         puts "Player page: http://www.rotoworld.com#{@players[input.to_i-1][:url]}"
+        puts ""
         puts "Would you like to view impact [y/n]?"
         yn = gets.strip.downcase
-            puts @players[input.to_i-1][:impact] if yn == "y"
+            if yn == "y"
+              puts ""
+              puts @players[input.to_i-1][:impact]
+            end
       elsif input == "list"
         list_players
       elsif input == "exit"
@@ -39,7 +46,9 @@ class NflNews::CLI
   end
   
   def goodbye
+    puts ""
     puts "Check back later for more news."
+    puts ""
   end
     
 end
